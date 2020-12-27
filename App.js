c47552db45dacdaaf6ae7129eedd56a9b2705dbe
Delice <ibrahimdelice96@gmail.com>
2020-12-27 18:40:39 +0100

    Corrected register screen navigation

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './Screens/LoginScreen';
 import Home from './Screens/HomeScreen';
+import Register from './Screens/RegisterScreen';
 
 const Stack = createStackNavigator();
 
@@ -19,7 +20,7 @@ export default function App() {
           options={{ title: 'Login' }}
         />
         <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Register" component={Home} />
+        <Stack.Screen name="Register" component={Register} />
       </Stack.Navigator>
     </NavigationContainer>
   );
