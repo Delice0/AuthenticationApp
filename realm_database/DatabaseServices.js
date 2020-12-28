ccbc9064e4176811e2f15d27b22f760d98673518
Delice <ibrahimdelice96@gmail.com>
2020-12-28 14:42:40 +0100

    Created database service
    - table
    - schema
    - create user function

---
+++ realm_database/DatabaseServices.js
@@ -0,0 +1,37 @@
+import Realm from 'realm';
+
+const USER_TABLE = 'User'
+
+const USER_SCHEMA = {
+    name: USER_TABLE,
+    primaryKey: 'id',
+    properties: {
+        id: 'int',
+        username: 'string',
+        password: 'string'
+    }
+}
+
+const DATABASE_OPTIONS = {
+    schema: [USER_SCHEMA],
+    schemaVersion: 0,
+    inMemory: true,
+}
+
+export const CreateUser = (id, username, password) => {
+    return (
+        Realm.open({
+            DATABASE_OPTIONS
+        }).then(realm => {
+            realm.write(() => {
+                realm.create(USER_TABLE, { id: id + 1, username: username, password: password });
+            })
+
+            if (realm !== null && !realm.isClosed) {
+                realm.close()
+            }
+        }).catch((error) => console.log('Something went wrong when creating user\n' + "Error: " + error))
+    )
+}
+
+export default new Realm(DATABASE_OPTIONS)
