d1317692530f25d1518b0dc03ae3e964e7ca3a29
Delice <ibrahimdelice96@gmail.com>
2021-01-02 22:32:57 +0100

    New refactorings and addings to login screen
    - Moved "onPressSubmit" logic out to a method
    - Added checkbox for "remember me" logic
    - Color "black" to title

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -1,8 +1,24 @@
+import CheckBox from '@react-native-community/checkbox';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { doesUserExist } from '../realm_database/DatabaseServices'
 
 const Login = ({ navigation }) => {
+
+    const [toggleRememberMe, setRememberMe] = React.useState(false)
+    const [inputUsername, setUsername] = React.useState('')
+    const [inputPassword, setPassword] = React.useState('')
+
+    const onPressSubmit = () => {
+        if (doesUserExist(inputUsername, inputPassword)) {
+            navigation.navigate('Home')
+            alert("Welcome " + inputUsername + "!")
+        } else {
+            alert("User does not exists!")
+        }
+    }
+
     return (
         <View style={{ marginTop: 50 }}>
             <Text
@@ -12,16 +28,37 @@ const Login = ({ navigation }) => {
             <TextInput
                 style={styles.input}
                 placeholder='Please enter username here..'
-                placeholderTextColor="#9a73ef" />
+                placeholderTextColor="#9a73ef"
+                onChangeText={(input) => setUsername(input)} />
 
             <TextInput
                 style={styles.input}
                 placeholder='Please enter password here..'
-                placeholderTextColor="#9a73ef" />
+                placeholderTextColor="#9a73ef"
+                onChangeText={(input) => setPassword(input)} />
+
+            <View style={{ flexDirection: 'row' }}>
+
+                <View style={{ flex: 1 }}>
+                    <CheckBox
+                        style={{ justifyContent: 'flex-end', marginLeft: 10 }}
+                        disabled={false}
+                        value={toggleRememberMe}
+                        onValueChange={(toggle) => setRememberMe(toggle)}>
+                    </CheckBox>
+                </View>
+
+                <View style={{ flex: 7, marginTop: 6 }}>
+                    <Text
+                        style={styles.rememberMeText}
+                    > Remember me</Text>
+                </View>
+
+            </View>
 
             <TouchableOpacity
                 style={styles.submitButton}
-                onPress={() => navigation.navigate('Home')}>
+                onPress={() => onPressSubmit()}>
 
                 <Text style={styles.submitButtonText}> Submit </Text>
             </TouchableOpacity>
@@ -41,6 +78,7 @@ const styles = StyleSheet.create({
     },
     credentialsText: {
         textAlign: 'center',
+        color: 'black',
         fontSize: 30,
         marginBottom: 30
     },
@@ -49,6 +87,10 @@ const styles = StyleSheet.create({
         height: 40,
         borderColor: '#7a42f4',
         borderWidth: 1
+    },
+    rememberMeText: {
+        justifyContent: 'flex-start',
+        color: '#7a42f4'
     },
     submitButton: {
         backgroundColor: '#7a42f4',
