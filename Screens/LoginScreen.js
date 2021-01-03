41bdb9e7e6edad4a7e8e4c89124dc7ecddc255b6
Delice <ibrahimdelice96@gmail.com>
2021-01-03 23:04:44 +0100

    Some major changes to login screen
    - HelperText implemented
    - Refactored login component to a class
    - Validation on username and password when trying to log in

--- Screens/LoginScreen.js
+++ Screens/LoginScreen.js
@@ -2,74 +2,130 @@ import CheckBox from '@react-native-community/checkbox';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import { HelperText } from 'react-native-paper';
 import { doesUserExist } from '../realm_database/DatabaseServices'
 
-const Login = ({ navigation }) => {
+class Login extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isRememberMeToggled: false,
+            inputUsername: '',
+            inputPassword: '',
+            showErrorOnUsernameValidation: false,
+            showErrorOnPasswordValidation: false,
+        }
+    }
 
-    const [toggleRememberMe, setRememberMe] = React.useState(false)
-    const [inputUsername, setUsername] = React.useState('')
-    const [inputPassword, setPassword] = React.useState('')
+    isUsernameInputFieldEmpty() {
+        return this.state.inputUsername.trim() === ''
+    }
 
-    const onPressSubmit = () => {
-        if (doesUserExist(inputUsername, inputPassword)) {
-            navigation.navigate('Home')
+    isPasswordInputFieldEmpty() {
+        return this.state.inputPassword.trim() === ''
+    }
+
+    validateUsernameInputField() {
+        this.setState({ showErrorOnUsernameValidation: this.isUsernameInputFieldEmpty() })
+    }
+
+    validatePasswordInputField() {
+        this.setState({ showErrorOnPasswordValidation: this.isPasswordInputFieldEmpty() })
+    }
+
+    showErrorIfUsernameNotValid() {
+        return <HelperText
+            style={styles.helperText}
+            type="error"
+            visible={this.state.showErrorOnUsernameValidation}>
+            Username cannot be empty! </HelperText>
+    }
+
+    showErrorIfPasswordNotValid() {
+        return <HelperText
+            style={styles.helperText}
+            type="error"
+            visible={this.state.showErrorOnPasswordValidation}>
+            Password cannot be empty! </HelperText>
+    }
+
+    onPressSubmit() {
+        const { inputUsername, inputPassword } = this.state
+
+        if (this.isUsernameInputFieldEmpty() || this.isPasswordInputFieldEmpty()) {
+            alert("Please fill both username and password!")
+        } else if (doesUserExist(inputUsername, inputPassword)) {
+            this.props.navigation.navigate('Home')
             alert("Welcome " + inputUsername + "!")
         } else {
             alert("User does not exists!")
         }
     }
 
-    return (
-        <View style={{ marginTop: 50 }}>
-            <Text
-                style={styles.credentialsText}>
-                Credentials </Text>
+    render() {
+        return (
+            <View style={{ marginTop: 50 }}>
+                <Text
+                    style={styles.credentialsText}>
+                    Credentials </Text>
 
-            <TextInput
-                style={styles.input}
-                placeholder='Please enter username here..'
-                placeholderTextColor="#9a73ef"
-                onChangeText={(input) => setUsername(input)} />
+                <View>
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Please enter username here..'
+                        placeholderTextColor="#9a73ef"
+                        onChangeText={(input) => this.setState({ inputUsername: input })} />
 
-            <TextInput
-                style={styles.input}
-                placeholder='Please enter password here..'
-                placeholderTextColor="#9a73ef"
-                onChangeText={(input) => setPassword(input)} />
-
-            <View style={{ flexDirection: 'row' }}>
-
-                <View style={{ flex: 1 }}>
-                    <CheckBox
-                        style={{ justifyContent: 'flex-end', marginLeft: 10 }}
-                        disabled={false}
-                        value={toggleRememberMe}
-                        onValueChange={(toggle) => setRememberMe(toggle)}>
-                    </CheckBox>
+                    {this.showErrorIfUsernameNotValid()}
                 </View>
 
-                <View style={{ flex: 7, marginTop: 6 }}>
-                    <Text
-                        style={styles.rememberMeText}
-                    > Remember me</Text>
+                <View>
+                    <TextInput
+                        style={styles.input}
+                        placeholder='Please enter password here..'
+                        placeholderTextColor="#9a73ef"
+                        onChangeText={(input) => this.setState({ inputPassword: input })} />
+
+                    {this.showErrorIfPasswordNotValid()}
                 </View>
 
+                <View style={{ flexDirection: 'row' }}>
+                    <View style={{ flex: 1 }}>
+                        <CheckBox
+                            style={{ justifyContent: 'flex-end', marginLeft: 10 }}
+                            disabled={false}
+                            value={this.state.isRememberMeToggled}
+                            onValueChange={(value) => this.setState({ isRememberMeToggled: value })} >
+                        </CheckBox>
+                    </View>
+
+                    <View style={{ flex: 7, marginTop: 6 }}>
+                        <Text
+                            style={styles.rememberMeText}
+                        > Remember me</Text>
+                    </View>
+                </View>
+
+                <View>
+                    <TouchableOpacity
+                        style={styles.submitButton}
+                        onPress={() => {
+                            this.validateUsernameInputField()
+                            this.validatePasswordInputField()
+                            this.onPressSubmit()
+                        }}>
+
+                        <Text style={styles.submitButtonText}> Submit </Text>
+                    </TouchableOpacity>
+                </View>
+
+                <Text
+                    style={styles.registerText}
+                    onPress={() => this.props.navigation.navigate('Register')}>
+                    Don't have an account? Register here! </Text>
             </View>
-
-            <TouchableOpacity
-                style={styles.submitButton}
-                onPress={() => onPressSubmit()}>
-
-                <Text style={styles.submitButtonText}> Submit </Text>
-            </TouchableOpacity>
-
-            <Text
-                style={styles.registerText}
-                onPress={() => navigation.navigate('Register')}>
-                Don't have an account? Register here!
-            </Text>
-        </View>
-    );
+        );
+    }
 }
 
 const styles = StyleSheet.create({
@@ -104,6 +160,10 @@ const styles = StyleSheet.create({
     registerText: {
         textAlign: 'center',
         color: '#7a42f4',
+    },
+    helperText: {
+        marginTop: -20,
+        marginLeft: 3.5,
     },
 });
 
